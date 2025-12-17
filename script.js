0c74c171c555d515dc5adc6cafe77ea01386ba5d
idrees067 <idreeskhan_ehsanyar@yahoo.com>
2025-12-18 01:28:21 +0500

    l

---
+++ script.js
@@ -0,0 +1,6 @@
+// Smooth scroll to Projects
+document.getElementById("viewWorkBtn").addEventListener("click", function () {
+  document.getElementById("projects").scrollIntoView({
+    behavior: "smooth",
+  });
+});
